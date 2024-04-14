bb2bbcd3472d0ff903a9a960fb2bd4666e012b57
Ketan Maurya <ketanmaurya@gmail.com>
2024-04-14 15:30:01 +0530

    Time: 44 ms (62.22%) | Memory: 17.9 MB (22.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,9 +2,8 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        string = s 
         pattern = re.compile('[\W_]+')
-        x = pattern.sub('', string.lower())
+        x = pattern.sub('', s.lower())
         y = len(x) - 1 
         for z in range(len(x)):
             start = x[z]
